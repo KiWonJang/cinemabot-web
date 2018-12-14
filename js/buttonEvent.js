0c55ba7796032eaf26ec70177d49b13b31686c43
KiWonJang <jkwonny93@ajou.ac.kr>
2018-12-14 13:35:28 +0900

    Design revision complete.

--- js/buttonEvent.js
+++ js/buttonEvent.js
@@ -1,72 +1,71 @@
 $(function(){
 
   function currentTime() {
-    var now = new Date();                                                  // 현재시간
+    var now = new Date();
     var nowTime =  now.getHours() + "시" + now.getMinutes() + "분" + now.getSeconds() + "초";
     return nowTime;
   }
 
   function sDivMsg(msg){
-    var now = new Date();                                                  // 현재시간
+    var now = new Date();
     var nowTime =  now.getHours() + "시" + now.getMinutes() + "분" ;
     return `<div class="row msg_container base_sent">
-        <div class="col-md-10 col-xs-10 ">
+        <div class="col-md-10 col-xs-10 message-inner-s">
             <div class="messages msg_sent">
                 <p>${msg}</p>
                 <time datetime="2009-11-13T20:00">${currentTime()}</time>
             </div>
         </div>
         <div class="col-md-2 col-xs-2 avatar">
-            <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive ">
+            <img src="image/profile.png" class=" img-responsive ">
         </div>
     </div> `;
   }
   function rDivMsg(msg){
-    var now = new Date();                                                  // 현재시간
+    var now = new Date();
     var nowTime =  now.getHours() + "시" + now.getMinutes() + "분" ;
     return `<div class="row msg_container base_receive">
         <div class="col-md-2 col-xs-2 avatar">
-            <img src="image/popcorn.png" class=" img-responsive ">
+            <img src="image/popcorn.png" class=" img-responsive">
         </div>
-        <div class="col-xs-10 col-md-10">
+        <div class="col-xs-10 col-md-10 message-inner-r">
             <div class="messages msg_receive">
                 <p>${msg}</p>
-                <time datetime="2009-11-13T20:00" id="clock"></time>
+                <time datetime="2009-11-13T20:00" id="clock" >${currentTime()}</time>
             </div>
         </div>
     </div> `;
   }
+  function tDivMsg(msg){
+    return `<div id="total">${msg}</div> `;
+  }
+
   var sList = [];
-  var rList = [];
-  var temp;
-  var h = 0;
 
   $('#sendBtn').click(function(){
-    $('.msg_container_base').scrollTop(320*sList.length);
-    alert($("#scroll").height());
     var msg = $('#inputText').val();
-    // list.push(inputText);
     sList.push(sDivMsg(msg));
-
     $('#show').html(sList);
+    var input = document.getElementById("inputText");
+    input.value = null;
     $.ajax({
         type : "POST",
         url : "http://54.180.98.17:8080/message",
-        // crossDomain: true,
         contentType:'application/json',
         // async : false,
         dataType :'json',
         data : JSON.stringify({content: msg}),
         success : function(message){
-            sList.push(rDivMsg(JSON.stringify(message)));
+            var s = JSON.stringify(message["message"]["text"]);
+            s = s.substring(1,s.length-1);
+            sList.push(tDivMsg(rDivMsg(s)));
             $('#show').html(sList);
+            $('.msg_container_base').scrollTop(320*sList.length);
         },
         error : function(e){
             sList.push(rDivMsg(JSON.stringify(e)));
             $('#show').html(sList);
-        },
+        }
     })
-
   })
-
 })
